3b0bd6664492f945ed9b71313511788f23e5da15
0639Quercus <thsomsits.brie@gmail.com>
2021-12-14 16:15:19 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 int main(){	
+	double PrevBalance, Interest, InterestRate, Total, Payment, MaxPayment, NewBalance;
+	int EndOfYear = 0;
+
 	cout << "Enter initial loan: ";
+	cin >> PrevBalance;
+
 	cout << "Enter interest rate per year (%): ";
+	cin >> InterestRate;
+	InterestRate /= 100;
+
 	cout << "Enter amount you can pay per year: ";
+	cin >> MaxPayment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +27,31 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	do {
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+		EndOfYear++;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << EndOfYear; 
+
+		cout << setw(13) << left << PrevBalance;
+
+		Interest = PrevBalance * InterestRate;
+		cout << setw(13) << left << Interest;
+
+		Total = PrevBalance + Interest;
+		cout << setw(13) << left << Total;
+
+		Payment = MaxPayment;
+		if (Total < MaxPayment) Payment = Total;
+		cout << setw(13) << left << Payment;
+
+		NewBalance = Total - Payment;
+		cout << setw(13) << left << NewBalance;
+		PrevBalance = NewBalance;
+
+		cout << "\n";	
+	} while (NewBalance > 0);
+
 	return 0;
 }
